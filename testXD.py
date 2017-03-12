f25d19646786db10c9a04650bb08f099eda25517
salomanders <landerson@simonsfoundation.org>
2017-03-12 11:21:32 -0400

    more gaussians

--- testXD.py
+++ testXD.py
@@ -875,10 +875,9 @@ if __name__ == '__main__':
                       'K':{'key':'k_mag', 'err_key':'k_cmsig', 'array':twoMass},
                       'G':{'key':'phot_g_mean_mag', 'array':tgas}}
 
-    #iteration = ['1st', '2nd', '3rd', '4th', '5th']
-    #previteration =  ['0th', '1st', '2nd', '3rd', '4th']
-    iteration = ['6th', '7th', '8th', '9th', '10th']
-    previteration = ['5th', '6th', '7th', '8th', '9th']
+    iteration = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
+    previteration =  ['0th', '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th']
+
     for iter, previter in zip(iteration, previteration):
 
         xdgmmFilename = 'xdgmm.'             + str(ngauss) + 'gauss.dQ' + str(quantile) + '.' + iter + '.' + survey + '.' + dataFilename + '.fit'
@@ -887,8 +886,10 @@ if __name__ == '__main__':
         priorFile     = 'prior.'             + str(ngauss) + 'gauss.dQ' + str(quantile) + '.' + iter + '.' + survey + '.' + dataFilename + '.png'
 
         if previter == '0th':
-            zerothDust =
-            dustEBV = np.zeros(np.sum(indices))
+            dustZeroFile = 'dustCorrection.128gauss.dQ0.05.5th.2MASS.All.npz'
+            data = np.load(dustZeroFile)
+            dustEBV = data['ebv']
+            #dustEBV = np.zeros(np.sum(indices))
         else:
             if not isinstance(dustEBV,np.ndarray):
                 dustFilePrev = 'dustCorrection.'    + str(ngauss) + 'gauss.dQ' +str(quantile) + '.' + previter + '.' + survey + '.' + dataFilename
