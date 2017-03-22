717b7face63ef40265e1366ca31064d2074aa16f
salomanders <landerson@simonsfoundation.org>
2017-03-21 20:48:07 -0400

    small tweaks, more tomorrow

--- toy/toy.py
+++ toy/toy.py
@@ -41,38 +41,26 @@ def denoise_one_datum(xn, yn, sigman, m, b, t):
     return (yn * s2inv + (m * xn + b) * t2inv) / (s2inv + t2inv), \
         np.sqrt(1. / (s2inv + t2inv))
 
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    import matplotlib as mpl
-    mpl.rcParams['xtick.labelsize'] = 18
-    mpl.rcParams['ytick.labelsize'] = 18
-    nexamples =  np.int(sys.argv[1])
-    xlim = (-1, 1)
-    ylim = (-5, 5)
-    fig, axes = plt.subplots(1, 3, figsize=(16, 5))
-    axes = axes.flatten()
-    mtrue, btrue, ttrue = -1.37, 0.2, 0.8
-    parsTrue = [mtrue, btrue, ttrue] #m, b, t
+def plot(fig, axes, figNoise, axesNoise, xns, yns, sigmans, yntrues, m, b, t, mtrue, btrue, ttrue, ydns, sigmadns, nexamples=5):
 
     alpha_all = 0.05
     alpha_chosen = 1.0
+    xlim = (-1, 1)
+    ylim = (-5, 5)
 
     dataMap = mpl.cm.get_cmap('Blues')
     dataColor = dataMap(0.75)
     trueMap = mpl.cm.get_cmap('Reds')
     trueColor = trueMap(0.75)
-    xns, yns, sigmans, yntrues = make_fake_data(parsTrue)
+
     for ax in axes[:-1]:
         ax.errorbar(xns, yns, yerr=sigmans, fmt="o", color="k", alpha=alpha_all ,mew=0)
         ax.errorbar(xns[0:nexamples], yns[0:nexamples], yerr=sigmans[0:nexamples], fmt="o", color="k", zorder=37, alpha=alpha_chosen, mew=0)
 
-
-    m, b, t = get_best_fit_pars(xns, yns, sigmans)
-    print m, b, t
     xp = np.array(xlim)
     axes[1].plot(xp, m * xp + b + t, color=dataColor)
     axes[1].plot(xp, m * xp + b - t, color=dataColor)
-    axes[1].scatter(xns[0:nexamples], yntrues[0:nexamples], c=trueColor, s=20., lw=2, zorder=36, alpha=alpha_chosen, facecolors='None')
+    axes[1].scatter(xns[0:nexamples], yntrues[0:nexamples], c=trueColor, lw=2, zorder=36, alpha=alpha_chosen, facecolors='None')
     axes[1].plot(xp, mtrue*xp + btrue + ttrue, color=trueColor, zorder=35)
     axes[1].plot(xp, mtrue*xp + btrue - ttrue, color=trueColor, zorder=34)
     r1 = axes[1].add_patch(mpl.patches.Rectangle((-10,-10), 0.1, 0.1, color='black', alpha=alpha_chosen))
@@ -80,11 +68,6 @@ if __name__ == "__main__":
     r3 = axes[1].add_patch(mpl.patches.Rectangle((-10,-10), 0.1, 0.1, color=dataColor, alpha=alpha_chosen))
     axes[1].legend((r1,r2,r3), ('data', 'truth', 'denoised'), loc='best', fontsize=12)
 
-    ydns = np.zeros_like(yns)
-    sigmadns = np.zeros_like(sigmans)
-    for n, (xn, yn, sigman) in enumerate(zip(xns, yns, sigmans)):
-        ydns[n], sigmadns[n] = denoise_one_datum(xn, yn, sigman, m, b, t)
-    #for ax in axes[1]:
     axes[1].errorbar(xns[0:nexamples], ydns[0:nexamples], yerr=sigmadns[0:nexamples], fmt="o", color=dataColor, zorder=37, alpha=alpha_chosen, mew=0)
 
     norm = mpl.colors.Normalize(vmin=0, vmax=9)
@@ -104,5 +87,52 @@ if __name__ == "__main__":
         ax.set_ylim(ylim)
         ax.set_xticks((-1, -0.5, 0, 0.5, 1))
         ax.set_xticklabels((-1, 0.5, 0, 0.5, 1))
-    plt.tight_layout()
-    fig.savefig("toy.png")
+    #plt.tight_layout()
+
+    axesNoise[0].hist(sigmans, bins=20, histtype='step', normed=True, lw=2)
+    axesNoise[0].set_xlabel(r'$\sigma_n$', fontsize=15)
+    axesNoise[1].hist(sigmadns, bins=20, histtype='step', normed=True, lw=2)
+    axesNoise[1].set_xlabel(r'$\tilde\sigma_n$', fontsize=15)
+    for a in axesNoise[0:2]:
+        a.axvline(t, label='$t$', color='black', lw=2)
+        a.legend(fontsize=15, loc='best')
+    axesNoise[2].hist(yns-yntrues, bins=20, histtype='step', normed=True, lw=2)
+    axesNoise[2].set_xlabel(r'$y_n - y_{true,n}$', fontsize=15)
+    axesNoise[3].hist(ydns-yntrues, bins=20, histtype='step', normed=True, lw=2)
+    axesNoise[3].set_xlabel(r'$<p(y_{true,n})> - y_{true,n}$', fontsize=15)
+    #plt.tight_layout()
+
+    return fig, axes, figNoise, axesNoise
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import matplotlib as mpl
+
+    nexamples =  np.int(sys.argv[1])
+    mtrue, btrue, ttrue = -1.37, 0.2, 0.8
+    parsTrue = [mtrue, btrue, ttrue] #m, b, t
+
+    xns, yns, sigmans, yntrues = make_fake_data(parsTrue)
+    m, b, t = get_best_fit_pars(xns, yns, sigmans)
+
+    print m, b, t
+
+    ydns = np.zeros_like(yns)
+    sigmadns = np.zeros_like(sigmans)
+    for n, (xn, yn, sigman) in enumerate(zip(xns, yns, sigmans)):
+        ydns[n], sigmadns[n] = denoise_one_datum(xn, yn, sigman, m, b, t)
+
+    for label, style in zip(['paper', 'talk'],['seaborn-paper', 'seaborn-talk']):
+
+        plt.style.use(style)
+        mpl.rcParams['xtick.labelsize'] = 18
+        mpl.rcParams['ytick.labelsize'] = 18
+        fig, axes = plt.subplots(1, 3, figsize=(16, 5))
+        axes = axes.flatten()
+        figNoise, axesNoise = plt.subplots(2,2, figsize=(8,8))
+        axesNoise = axesNoise.flatten()
+
+        fig, axes, figNoise, axesNoise = plot(fig, axes, figNoise, axesNoise, xns, yns, sigmans, yntrues, m, b, t, mtrue, btrue, ttrue, ydns, sigmadns, nexamples=nexamples)
+        figNoise.tight_layout()
+        figNoise.savefig('toyNoise.' + label + '.png')
+        fig.savefig("toy." + label + ".png")
