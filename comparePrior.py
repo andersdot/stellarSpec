9e865c970286ac6172c3063ab29669abecaf9e90
salomanders <landerson@simonsfoundation.org>
2017-03-30 18:54:27 -0400

    example plots

--- comparePrior.py
+++ comparePrior.py
@@ -252,7 +252,7 @@ def examplePosterior(nexamples=100, postFile='posteriorSimple.npz', dustFile='du
         meanPrior = meanPrior[0]
         covPrior = covPrior[0]
         allMeans, allAmps, allCovs, summedPriorAbsmagKinda = testXD.absMagKindaPosterior(xdgmm, ndim, meanPrior, covPrior, xabsMagKinda, projectedDimension=1, nPosteriorPoints=nPosteriorPoints, prior=True)
-        priorParallax = summedPriorAbsMagKinda*10.**(0.2*apparentMagnitude[index])
+        priorParallax = summedPriorAbsMagKinda*10.**(0.2*apparentMagnitude[i])
         plt.clf()
         plt.plot(xparallaxMAS, posterior[i], label='posterior')
         plt.plot(xparallaxMAS, st.gaussian(tgas['parallax'][i], tgas['parallax_error'][i], xparallaxMAS), label='likelhood')
