859955ee9a90a8edde05cef780c68f72f03814bc
salomanders <landerson@simonsfoundation.org>
2017-04-02 17:42:55 -0400

    tweaking plots

--- comparePrior.py
+++ comparePrior.py
@@ -325,9 +325,9 @@ def compareSimpleGaia(ngauss=128, quantile=0.05, iter='10th', survey='2MASS', da
         fig.savefig(file.split('.')[0] + '_Comparison2Gaia.png')
         notnans = ~np.isnan(var) & ~np.isnan(tgas['parallax_error'])
         print 'The median of the differences of the logs: ', np.median(np.log(var[notnans]) - np.log(tgas['parallax_error'][notnans]**2.))
-
-        fig, ax = plt.subplots(1, 2, figsize=(12, 5))
-        ax[0].scatter(color[notnans], np.log(var[notnans]) - np.log(tgas['parallax_error'][notnans]**2.), lw=0, s=1, alpha=0.5, c='black')
+        cNorm  = plt.matplotlib.colors.Normalize(vmin=-6, vmax=6)
+        fig, ax = plt.subplots(1, 2, figsize=(14, 6))
+        ax[0].scatter(color[notnans], np.log(var[notnans]) - np.log(tgas['parallax_error'][notnans]**2.), lw=0, s=1, alpha=0.5, c=tesXD.absMagKinda2absMag(absMagKinda[notnans]), norm=cNorm, cmap='plasma')
         ax[0].set_xlabel(r'$(J-K)^c$', fontsize=18)
         ax[0].set_ylim(-6, 2)
         ax[0].set_xlim(-1, 3)
